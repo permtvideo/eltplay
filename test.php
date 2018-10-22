812d1ad48109724a15fb21c09abc47704efd0e1b
permtvideo <43233238+permtvideo@users.noreply.github.com>
2018-10-22 20:00:41 +0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<?
+$queryUrl = 'https://ultralive.bitrix24.ru/rest/11/bdb8uef8w3f9orml/user.current.json';
+$queryData = http_build_query(array(
+     		type: 'user',
+     	 	ownerId: '11',
+      		name: 'test',
+      		description: 'Description for event',
+      		from: '2018-10-24',
+      		to: '2018-10-24',
+      		skipTime: 'Y',
+      		section: 1,
+));   
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_SSL_VERIFYPEER => 0,
+  CURLOPT_POST => 1,
+  CURLOPT_HEADER => 0,
+  CURLOPT_RETURNTRANSFER => 1,
+  CURLOPT_URL => $queryUrl,
+));
+
+ 
