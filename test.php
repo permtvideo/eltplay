30d527aee99b8f22cc0c6d3a1c8cce25962a74a8
permtvideo <43233238+permtvideo@users.noreply.github.com>
2018-10-22 20:11:55 +0500

    Update test.php

--- test.php
+++ test.php
@@ -1,25 +1,30 @@
 <?
-$queryUrl = 'https://ultralive.bitrix24.ru/rest/11/bdb8uef8w3f9orml/calendar.event.add.json';
-$queryData = http_build_query(array(
-     		type: 'user',
-     	 	ownerId: '11',
-      		name: 'test',
-      		description: 'Description for event',
-      		from: '2018-10-24',
-      		to: '2018-10-24',
-      		skipTime: 'Y',
-      		section: 1,
-));   
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_SSL_VERIFYPEER => 0,
-  CURLOPT_POST => 1,
-  CURLOPT_HEADER => 0,
-  CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => $queryUrl,
-));
+$queryUrl = 'https://ultralive.bitrix24.ru/rest/11/bdb8uef8w3f9orml/user.current.json';
+    
+// $queryData = http_build_query(array());
 
+   
+ $curl = curl_init();
+ curl_setopt_array($curl, array(
+   CURLOPT_SSL_VERIFYPEER => 0,
+   CURLOPT_POST => 1,
+   CURLOPT_HEADER => 0,
+   CURLOPT_RETURNTRANSFER => 1,
+   CURLOPT_URL => $queryUrl,
+  //CURLOPT_POSTFIELDS => $queryData,
+    ));
+
+ 
+$result = curl_exec($curl);
 curl_close($curl);
+
+
+  
+echo "<pre>";
+print_r($result);
+print_r(json_decode($result, true));
+echo "</pre>";
+
 
 
 
